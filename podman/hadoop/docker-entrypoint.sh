09273e0fe62343980216c5013085bfa3775b52fc
Christoph Kappel <christoph@unexist.dev>
2022-12-21 16:50:00 +0100

    Reordered scripts

--- podman/hadoop/docker-entrypoint.sh
+++ podman/hadoop/docker-entrypoint.sh
@@ -1,49 +1,46 @@
 #!/bin/bash
 set -e
 
-if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
-  # Generate fresh RSA key
-  doas ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N "" -t rsa
-fi
-if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
-  # Generate fresh DSA key
-  doas ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N "" -t dsa
-fi
-
-echo "Start sshd"
+echo "Starting sshd"
 doas /usr/sbin/sshd -D >/dev/null &
 
+# Format namenodes
 if [[ -z ${FORMATNODES} || ${FORMATNODES} -ne 0 ]]; then
-  echo "Format namenode"
-  ${HADOOP_HOME}/bin/hdfs namenode -format && echo "Format namenode: OK"
+    echo "Formatting namenode"
+    ${HADOOP_HOME}/bin/hdfs namenode -format
 
-  echo "Format secondarynamenode"
-  ${HADOOP_HOME}/bin/hdfs secondarynamenode -format -checkpoint force && echo "Format secondarynamenode: OK"
+    #echo "Formatting secondarynamenode"
+    #${HADOOP_HOME}/bin/hdfs secondarynamenode -format -checkpoint force
+else
+    echo "Not formatting nodes"
 fi
 
-# Create paths
-mkdir -p ${HADOOP_HOME}/hdfs/name
-mkdir -p ${HADOOP_HOME}/hdfs/data
-mkdir -p ${HADOOP_HOME}/logs
-chmod -R 755 ${HADOOP_HOME}/hdfs/
-
-echo "Start dfs nodes"
+echo "Starting nodes"
 ${HADOOP_HOME}/sbin/start-dfs.sh
 
-echo "YARNSTART = $YARNSTART"
-
 if [[ -z ${YARNSTART} || ${YARNSTART} -ne 0 ]]; then
-    echo "Start yarn"
+    echo "Starting yarn"
     ${HADOOP_HOME}/sbin/start-yarn.sh
+else
+    echo "Not starting yarn"
+fi
+
+if [[ -z ${MAPREDUCESTART} || ${MAPREDUCESTART} -ne 0 ]]; then
+    echo "Starting historyserver"
+    ${HADOOP_HOME}/bin/mapred --daemon start historyserver
+else
+    echo "Not starting historyserver"
 fi
 
 # Setup FS
-${HADOOP_HOME}/bin/hdfs dfs -mkdir /tmp
-${HADOOP_HOME}/bin/hdfs dfs -mkdir /users
-${HADOOP_HOME}/bin/hdfs dfs -mkdir /jars
-${HADOOP_HOME}/bin/hdfs dfs -chmod 777 /tmp
-${HADOOP_HOME}/bin/hdfs dfs -chmod 777 /users
-${HADOOP_HOME}/bin/hdfs dfs -chmod 777 /jars
+if [[ -z ${FORMATNODES} || ${FORMATNODES} -ne 0 ]]; then
+    ${HADOOP_HOME}/bin/hdfs dfs -mkdir /tmp
+    ${HADOOP_HOME}/bin/hdfs dfs -mkdir /users
+    ${HADOOP_HOME}/bin/hdfs dfs -mkdir /jars
+    ${HADOOP_HOME}/bin/hdfs dfs -chmod 777 /tmp
+    ${HADOOP_HOME}/bin/hdfs dfs -chmod 777 /users
+    ${HADOOP_HOME}/bin/hdfs dfs -chmod 777 /jars
+fi
 
 ${HADOOP_HOME}/bin/hdfs dfsadmin -safemode leave
 
