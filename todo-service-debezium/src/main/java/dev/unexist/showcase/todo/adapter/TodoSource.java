18aca76d04e194eeb943568ce5121b0fbe556c68
Christoph Kappel <christoph@unexist.dev>
2023-02-13 17:37:15 +0100

    Added messaging source

---
+++ todo-service-debezium/src/main/java/dev/unexist/showcase/todo/adapter/TodoSource.java
@@ -0,0 +1,37 @@
+/**
+ * @package Showcase-Hadoop-CDC-Quarkus
+ *
+ * @file Todo source
+ * @copyright 2023 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+package dev.unexist.showcase.todo.adapter;
+
+import dev.unexist.showcase.todo.domain.todo.Todo;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Message;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class TodoSource {
+
+    @Inject
+    @Channel("todo-created")
+    Emitter<Todo> emitter;
+
+    /**
+     * Send {@link Todo} to topic
+     *
+     * @param  todo  A {@link Todo} to convert and send
+     **/
+
+    public void send(Todo todo) {
+        this.emitter.send(Message.of(todo));
+    }
+}
