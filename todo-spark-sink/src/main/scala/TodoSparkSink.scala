06f4919c36a3d67e0e95397b709212e9c1bfe7b0
Christoph Kappel <christoph@unexist.dev>
2023-02-16 09:50:27 +0100

    Updated configs and added error suppression

--- todo-spark-sink/src/main/scala/TodoSparkSink.scala
+++ todo-spark-sink/src/main/scala/TodoSparkSink.scala
@@ -1,42 +1,61 @@
+/**
+ * @package Showcase-Hadoop-CDC-Quarkus
+ *
+ * @file Spark sink
+ * @copyright 2020-2022 Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.Trigger
 
 import java.util.concurrent.TimeUnit
 
-def main(args: Array[String]): Unit = {
+object TodoSparkSink {
+  def main(args: Array[String]): Unit = {
 
-  /* Configure the parameters for the catalog */
-  val sparkConf = new SparkConf()
-  
-  sparkConf.set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-  sparkConf.set("spark.sql.catalog.todo_catalog", "org.apache.iceberg.spark.SparkCatalog")
-  sparkConf.set("spark.sql.catalog.todo_catalog.type", "hadoop")
-  sparkConf.set("spark.sql.catalog.todo_catalog.warehouse", "hdfs://localhost:9000/warehouse")
+    /* Configure the parameters for the catalog */
+    val sparkConf = new SparkConf()
 
-  val spark = SparkSession
-    .builder()
-    .config(sparkConf)
-    .appName("TodoSparkSink")
-    .getOrCreate()
+    sparkConf.set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+    sparkConf.set("spark.sql.catalog.todo_catalog", "org.apache.iceberg.spark.SparkCatalog")
+    sparkConf.set("spark.sql.catalog.todo_catalog.type", "hadoop")
+    sparkConf.set("spark.sql.catalog.todo_catalog.warehouse", "hdfs://localhost:9000/warehouse")
 
-  /* Read data from the Kafka cluster */
-  val df = spark.readStream
-    .format("kafka")
-    .option("kafka.bootstrap.servers", "localhost:9092")
-    .option("subscribe", "todo_created")
-    .load()
+    val spark = SparkSession
+      .builder()
+      .config(sparkConf)
+      .appName("TodoSparkSink")
+      .getOrCreate()
 
-  val resDF = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-    .as[(String, String)].toDF("id", "data")
+    import spark.implicits._
 
-  /* Write data to the Iceberg table in streaming mode */
-  val query = resDF.writeStream
-    .format("iceberg")
-    .outputMode("append")
-    .trigger(Trigger.ProcessingTime(1, TimeUnit.MINUTES))
-    .option("path", "dlf_catalog.iceberg_db.iceberg_table")
-    .start()
+    /* Read data from the Kafka cluster */
+    val df = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "todo_created")
+      .load()
 
-  query.awaitTermination()
+    /* Field annotations just work for the direct field */
+    @SuppressFBWarnings(value = Array("BC_UNCONFIRMED_CAST_OF_RETURN_VALUE"), justification = "I don't know what I am doing")
+    val dataFrame = df.selectExpr("CAST(title AS STRING)", "CAST(description AS STRING)")
+
+    val resDF = dataFrame.as[(String, String)].toDF("title", "description")
+
+    /* Write data to the Iceberg table in streaming mode */
+    val query = resDF.writeStream
+      .format("iceberg")
+      .outputMode("append")
+      .trigger(Trigger.ProcessingTime(1, TimeUnit.MINUTES))
+      .option("path", "todo_catalog.spark.todos")
+      .start()
+
+    query.awaitTermination()
+  }
 }
