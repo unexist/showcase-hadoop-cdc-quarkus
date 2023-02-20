30f31c78619782ad72c389bfa3879aa82ee55ace
Christoph Kappel <christoph@unexist.dev>
2023-02-20 15:11:25 +0100

    Fixed accidentally added word..

--- todo-spark-sink/src/main/scala/dev/unexist/showcase/todo/TodoSparkSink.scala
+++ todo-spark-sink/src/main/scala/dev/unexist/showcase/todo/TodoSparkSink.scala
@@ -11,7 +11,7 @@
 
 package dev.unexist.showcase.todo
 
-Remov//import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
+//import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.Trigger
