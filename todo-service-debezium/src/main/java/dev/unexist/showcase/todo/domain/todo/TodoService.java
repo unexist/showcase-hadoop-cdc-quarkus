b83ec613351ee5b312450b5469be6189c3e89385
Christoph Kappel <christoph@unexist.dev>
2023-02-22 12:19:17 +0100

    Removed unused imports

--- todo-service-debezium/src/main/java/dev/unexist/showcase/todo/domain/todo/TodoService.java
+++ todo-service-debezium/src/main/java/dev/unexist/showcase/todo/domain/todo/TodoService.java
@@ -11,9 +11,6 @@
 
 package dev.unexist.showcase.todo.domain.todo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
