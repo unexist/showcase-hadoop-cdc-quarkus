660911861b4478232f3ac39ebf7e80ae93a80a4e
Christoph Kappel <christoph@unexist.dev>
2023-08-16 17:41:08 +0200

    Added sub-service

---
+++ todo-service-hadoop/src/main/java/dev/unexist/showcase/todo/infrastructure/persistence/HadoopTodoRepository.java
@@ -0,0 +1,64 @@
+/**
+ * @package Showcase-Hadoop-CDC-Quarkus
+ *
+ * @file Todo repository
+ * @copyright 2023-present Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.infrastructure.persistence;
+
+import dev.unexist.showcase.todo.domain.todo.Todo;
+import dev.unexist.showcase.todo.domain.todo.TodoRepository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class HadoopTodoRepository implements TodoRepository {
+
+    @Inject
+    EntityManager entityManager;
+
+    @Override
+    public boolean add(Todo todo) {
+        this.entityManager.persist(todo);
+
+        return true;
+    }
+
+    @Override
+    public boolean update(Todo todo) {
+        this.entityManager.persist(todo);
+
+        return true;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        findById(id).ifPresent(todo -> this.entityManager.remove(todo));
+
+        return true;
+    }
+
+    @Override
+    public List<Todo> getAll() {
+        return this.entityManager.createNamedQuery(Todo.FIND_ALL, Todo.class)
+                .getResultList();
+    }
+
+    @Override
+    public Optional<Todo> findById(int id) {
+        return this.entityManager.createNamedQuery(Todo.FIND_BY_ID, Todo.class)
+                .setParameter("id", id)
+                .getResultList()
+                .stream()
+                .findFirst();
+    }
+}
