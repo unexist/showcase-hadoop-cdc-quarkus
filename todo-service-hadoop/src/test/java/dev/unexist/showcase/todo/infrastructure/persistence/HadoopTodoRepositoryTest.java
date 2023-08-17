c624d9fcdef4a893f27b208a33ff9a7347bfdde0
Christoph Kappel <christoph@unexist.dev>
2023-08-17 14:24:28 +0200

    Added simple test case

--- todo-service-hadoop/src/test/java/dev/unexist/showcase/todo/infrastructure/persistence/HadoopTodoRepositoryTest.java
+++ todo-service-hadoop/src/test/java/dev/unexist/showcase/todo/infrastructure/persistence/HadoopTodoRepositoryTest.java
@@ -11,21 +11,56 @@
 
 package dev.unexist.showcase.todo.infrastructure.persistence;
 
+import dev.unexist.showcase.todo.domain.todo.DueDate;
+import dev.unexist.showcase.todo.domain.todo.Todo;
+import dev.unexist.showcase.todo.domain.todo.TodoRepository;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.BeforeAll;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @QuarkusTest
 @QuarkusTestResource(value = HadoopResource.class, restrictToAnnotatedClass = true)
 public class HadoopTodoRepositoryTest {
 
-    @BeforeAll
-    public void setUp() {
-
-    }
+    @Inject
+    TodoRepository repository;
 
     @Test
-    public void shouldGetEmptytestTodoEndpoint() {
+    public void shouldAddToAndGetFromRepository() {
+        Todo todo_in = createTodo();
+
+        assertThat(this.repository.add(todo_in)).isTrue();
+
+        List<Todo> allTodos = this.repository.getAll();
+
+        assertThat(allTodos).hasSize(1);
+        assertThat(todo_in).isEqualTo(allTodos.get(0));
+    }
+
+    private static Todo createTodo() {
+        Todo todo = new Todo();
+
+        todo.setId(0);
+        todo.setTitle("string");
+        todo.setDescription("string");
+
+        DueDate dueDate = new DueDate();
+
+        LocalDate date = LocalDate.from(
+                DateTimeFormatter.ofPattern(DueDate.DATE_PATTERN).parse("2021-05-07"));
+
+        dueDate.setStart(date);
+        dueDate.setDue(date);
+
+        todo.setDueDate(dueDate);
+
+        return todo;
     }
 }
