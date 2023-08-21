2f2724e6e3841425640e5e0841038d562c9e36be
Christoph Kappel <christoph@unexist.dev>
2023-08-21 16:55:47 +0200

    Added dummy Iceberg implementation

---
+++ todo-service-hadoop/src/main/java/dev/unexist/showcase/todo/infrastructure/persistence/HadoopIcebergTodoRepository.java
@@ -0,0 +1,130 @@
+/**
+ * @package Showcase-Hadoop-CDC-Quarkus
+ *
+ * @file Todo Hadoop Iceberg repository
+ * @copyright 2023-present Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.infrastructure.persistence;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.unexist.showcase.todo.domain.todo.Todo;
+import dev.unexist.showcase.todo.domain.todo.TodoRepository;
+import org.apache.commons.lang3.NotImplementedException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+@Named("hadoop_iceberg")
+public class HadoopIcebergTodoRepository implements TodoRepository {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HadoopIcebergTodoRepository.class);
+
+    private final String HADOOP_FILE = "/warehouse/quarkus/todo.txt";
+
+    @ConfigProperty(name = "hadoop.defaultFS")
+    String defaultFS;
+
+    ObjectMapper mapper;
+
+    Configuration configuration;
+
+    /**
+     * Constructor
+     */
+
+    public HadoopIcebergTodoRepository() {
+        this.mapper = new ObjectMapper();
+        this.configuration = new Configuration();
+
+        this.configuration.set("fs.defaultFS", this.defaultFS);
+    }
+
+    @Override
+    public boolean add(Todo todo) {
+        boolean retVal = false;
+
+        /* Append our todo as string */
+        try(FileSystem fileSystem = FileSystem.get(this.configuration)) {
+            Path hdfsPath = new Path(HADOOP_FILE);
+
+            FSDataOutputStream fsOut = fileSystem.append(hdfsPath);
+
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fsOut, StandardCharsets.UTF_8));
+
+            mapper.writeValue(bufferedWriter, todo);
+
+            bufferedWriter.close();
+
+            retVal = true;
+        } catch (IOException e) {
+            LOGGER.error("Cannot write data to HDFS", e);
+        }
+
+        return retVal;
+    }
+
+    @Override
+    public boolean update(Todo todo) {
+        throw new NotImplementedException("Needs to be implemented later");
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        throw new NotImplementedException("Needs to be implemented later");
+    }
+
+    @Override
+    public List<Todo> getAll() {
+        List<Todo> retVal = new java.util.ArrayList<>(Collections.emptyList());
+
+        try(FileSystem fileSystem = FileSystem.get(this.configuration)) {
+            Path hdfsPath = new Path(HADOOP_FILE);
+
+            FSDataInputStream inputStream = fileSystem.open(hdfsPath);
+
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
+            String line = null;
+            while (null != (line = bufferedReader.readLine()))
+            {
+                LOGGER.debug("Read line: %s", line);
+
+                retVal.add(this.mapper.readValue(line, Todo.class));
+            }
+
+            inputStream.close();
+        } catch (IOException e) {
+            LOGGER.error("Cannot read data from HDFS: ", e);
+        }
+
+        return retVal;
+    }
+
+    @Override
+    public Optional<Todo> findById(int id) {
+        throw new NotImplementedException("Needs to be implemented later");
+    }
+}
