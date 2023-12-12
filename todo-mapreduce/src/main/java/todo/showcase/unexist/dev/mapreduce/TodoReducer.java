6a2f8d475641cab644fb4856355a380227de2ef8
Christoph Kappel <christoph@unexist.dev>
2023-12-12 16:06:16 +0100

    Added mapper and reducer implementations

---
+++ todo-mapreduce/src/main/java/todo/showcase/unexist/dev/mapreduce/TodoReducer.java
@@ -0,0 +1,28 @@
+/**
+ * @package Showcase-Hadoop-CDC-Quarkus
+ *
+ * @file Todo reducer
+ * @copyright 2023-present Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package todo.showcase.unexist.dev.mapreduce;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class TodoReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws java.io.IOException,
+            InterruptedException {
+        int sum = 0;
+        for (IntWritable value : values) {
+            sum += value.get();
+        }
+        context.write(key, new IntWritable(sum));
+    }
+}
