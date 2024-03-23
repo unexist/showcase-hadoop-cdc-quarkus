894868bab114ace9dfedf6ad93bb7ca4c428e0b1
Christoph Kappel <christoph@unexist.dev>
2024-03-23 19:48:15 +0100

    Updated submit script

--- todo-spark-sink/submit.sh
+++ todo-spark-sink/submit.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
-spark-submit --master spark://localhost:7077 \
+#!/bin/zsh
+spark-submit --master spark://${HOST}:7077 \
     --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1 \
-    --conf spark.executorEnv.JAVA_HOME=/opt/java/openjdk \
-    --conf spark.yarn.appMasterEnv.JAVA_HOME=/opt/java/openjdk \
+    --conf spark.executorEnv.JAVA_HOME=${JAVA_HOME} \
+    --conf spark.yarn.appMasterEnv.JAVA_HOME=${JAVA_HOME} \
     --conf spark.sql.streaming.checkpointLocation=/tmp/checkpoint \
+    --conf spark.dynamicAllocation.enabled=false \
     --name todosink \
     --deploy-mode client \
     --num-executors 1 \
-    --class dev.unexist.showcase.todo.TodoSparkSinkSimple \
-    ./todo-spark-sink/target/todo-spark-sink-0.1.jar
+    --executor-cores 1 \
+    --driver-memory 1G \
+    --executor-memory 1G \
+    --class dev.unexist.showcase.todo.TodoSparkSinkToConsole \
+    ./target/todo-spark-sink-0.1.jar
